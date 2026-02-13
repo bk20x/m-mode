426518bd3a6427ec19bfad163f01d1ac7cc531a3
bk20x <bkaenel4@gmail.com>
2026-02-12 18:46:38 -0600

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# m-mode
+### this is incomplete but working; as of right now it only highlights the languages special forms
---
+++ m-mode.el
@@ -0,0 +1,21 @@
+(defvar m-keywords
+  '("load" "open" "define" "macro" "let" "let*" "if"
+    "each" "interned-symbols" "setf" "setq" "->" "doTimes" "macroexpand" "safe"
+    "who" "return")
+  "Special forms for M.")
+
+(defvar m-font-lock-defaults
+  `(((,(regexp-opt m-keywords 'symbols) . font-lock-keyword-face)
+     ;; Highlight (define | macro Name ...) 
+     ("(\\s-*\\(?:define\\|macro\\)\\s-+\\(\\(\\w\\|\\s_\\)+\\)" 1 font-lock-function-name-face)
+)))
+
+;;;###autoload
+(define-derived-mode m-mode lisp-mode "M"
+  "Major mode for editing M source files."
+  (setq font-lock-defaults m-font-lock-defaults))
+
+;;;###autoload
+(add-to-list 'auto-mode-alist '("\\.m\\'" . m-mode))
+
+(provide 'm-mode)
